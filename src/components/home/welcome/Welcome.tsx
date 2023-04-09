ca4afc81a57107fc610c932bb640724fa5b8c279
Harrison Pan <hanshen36@gmail.com>
2023-04-09 18:53:46 +1200

    feat: :sparkles: wip: Home - Welcome component

--- src/components/home/welcome/Welcome.tsx
+++ src/components/home/welcome/Welcome.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
 import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { FlatList, Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-import styles from './welcome.style';
 import { icons, SIZES } from '../../../constants';
+import styles from './welcome.style';
+
+const jobTypes = ['Full-time', 'Part-time', 'Contractor'];
 
 const Welcome: React.FC = (): JSX.Element => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time');
+
   return (
     <View>
       <View style={styles.container}>
@@ -15,8 +19,44 @@ const Welcome: React.FC = (): JSX.Element => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput}></TextInput>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {
+              throw new Error('Function not implemented.');
+            }}
+            placeholder="What are you looking for?"
+          />
         </View>
+
+        <TouchableOpacity
+          style={styles.searchBtn}
+          onPress={() => {
+            throw new Error('Function not implemented.');
+          }}
+        >
+          <Image source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/jobs/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
   );
