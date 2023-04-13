8babfd5a1f4aa172bd56b825dbc206821a97757f
Harrison Pan <hanshen36@gmail.com>
2023-04-14 01:27:14 +1200

    feat: :sparkles: add env properties for API

---
+++ src/env/env.ts
@@ -0,0 +1,5 @@
+export default {
+  JSEARCH_API_URL: 'https://jsearch.p.rapidapi.com',
+  RAPID_API_KEY: '5c3728b1a3msh75e01c7de53c60ap1c2147jsn6de92c4344ef',
+  RAPID_API_HOST: 'jsearch.p.rapidapi.com',
+};
