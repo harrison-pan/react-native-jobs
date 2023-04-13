c95b8a2420426c93a42aa30d7c78afc1b3823193
Harrison Pan <hanshen36@gmail.com>
2023-04-14 01:13:54 +1200

    build: :package: update addons and libraries

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,14 @@ module.exports = function (api) {
       '@babel/plugin-proposal-export-namespace-from',
       'react-native-reanimated/plugin',
       require.resolve('expo-router/babel'),
+      [
+        'module-resolver',
+        {
+          alias: {
+            src: './src',
+          },
+        },
+      ],
     ],
   };
 };
